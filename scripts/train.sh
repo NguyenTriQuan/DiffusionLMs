e5686fe3ba23ca95f4bea91b488fef5a345ff106
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-13 20:09:10 +0700

    diff

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@ python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_
 --diff_steps 1000 \
 --lr 0.0001 \
 --learning_steps 10000 \
---save_interval 1000 \
+--save_interval 5000 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
