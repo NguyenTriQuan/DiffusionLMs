484f8e73706b0810e3ef7b178bdffaaee4913319
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-14 07:21:43 +0700

    diff

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,4 +1,4 @@
 python -u run_decode.py \
---model_dir diffusion_models/{name-of-model-folder} \
+--model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t1000_sqrt_lossaware_seed102_test-qqp20230813-20:09:44 \
 --seed 123 \
 --split test
